7a39c0c1c7c3b77c5b7871dacfe6f9c6f0b598de
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-29 10:31:21 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int n) {
+        if (n%2 == 0) {
+            int[] answer = new int[n/2];
+            int index = 0;
+            for (int i = 1; i <= n; i++) {
+                if (i%2==1) {
+                    answer[index] = i;
+                    index++;
+                }
+            }
+            return answer;
+        }
+        else {
+            int[] answer = new int[n/2+1];
+            int index = 0;
+            for (int i = 1; i <= n; i++) {
+                if (i%2==1) {
+                    answer[index] = i;
+                    index++;
+                }
+            }
+            return answer;
+        }  
+    }
+}
