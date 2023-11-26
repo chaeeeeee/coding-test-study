1ef1b3aba71422975890c1bb6a81c401a7a65b5a
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-11-26 11:41:04 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 0.36 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        int[] result = new int[5];
+        
+        Arrays.sort(num_list);
+        for (int i = 0; i < 5; i++) {
+            result[i] = num_list[i];
+        }
+        
+        return result;
+    }
+}
