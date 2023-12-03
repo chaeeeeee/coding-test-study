3cb9a9de368dffb1213c73e979aa281b4e88fe1c
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-12-03 19:23:15 +0900

    [level unrated] Title: qr code, Time: 13.50 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        
+        String answer = "";
+        
+        for (int i = 0; i < code.length(); i++) {
+            char ch = code.charAt(i);
+            
+            if (i % q == r) {
+                answer += ch;
+            }
+        }
+        
+        return answer;
+    }
+}
