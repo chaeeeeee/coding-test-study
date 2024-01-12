51f75924fe109e8e9bf94fb903c937987fd04338
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-01-12 21:33:58 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    	int score;
+    	
+        Scanner sc = new Scanner(System.in);
+        score = sc.nextInt();
+        
+        if (score > 89) {
+        	System.out.println("A");
+        }
+        else if (score > 79) {
+        	System.out.println("B");
+        }
+        else if (score > 69) {
+        	System.out.println("C");
+        }
+        else if (score > 59) {
+        	System.out.println("D");
+        }
+        else {
+        	System.out.println("F");
+        }
+    }
+}
