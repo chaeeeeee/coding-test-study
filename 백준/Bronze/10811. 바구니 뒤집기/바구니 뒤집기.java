2321668c63bdeb2e2721bb4c25fd07679b1f60fa
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-01-16 21:45:42 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 256 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int [] B = new int[N+1];	//가방 배열
+			
+		for (int k = 1; k <= N; k++) {
+			B[k] = k;	//바구니에 공 1개씩 바구니 번호와 동일하게
+		}
+		
+		for (int k = 0; k < M; k++) {
+			int i = sc.nextInt();
+			int j = sc.nextInt();
+			
+			while (i < j) {
+				int temp = B[i];
+				B[i] = B[j];
+				B[j] = temp;
+				
+				i++;
+				j--;
+			}
+		}
+		
+		for (int k = 1; k <= N; k++) {
+			System.out.print(B[k] + " ");
+		}
+	}
+}
