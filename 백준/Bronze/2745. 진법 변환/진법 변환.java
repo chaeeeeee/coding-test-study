f5d0aa7e6672b034f03dd139e165114fd755fe39
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-01-23 22:30:18 +0900

    [Bronze II] Title: 진법 변환, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int N = sc.nextInt();
+
+        int num = 0;
+        long result = 0;
+        int idx = 0;
+
+        for (int i = str.length()-1; i >= 0; i--) {
+            char ch = str.charAt(i);
+
+            if (ch >= '0' && ch <= '9') {
+                num = ch - '0';
+            }
+            else {
+                num = ch - 55;
+            }
+
+            result += num * Math.pow(N, idx++);
+        }
+        System.out.println(result);
+    }
+}
