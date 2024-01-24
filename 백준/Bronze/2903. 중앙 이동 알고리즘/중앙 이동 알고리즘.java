c4fb7a43391e497fd7a19627fe8a8dee4c151a20
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-01-24 23:24:06 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 204 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int result = (int)Math.pow(2, N) + 1;
+        System.out.println(result * result);
+    }
+}
