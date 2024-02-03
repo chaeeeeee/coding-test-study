b09006347f7a63888ad843845d1faeacd78c09f9
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-02-03 22:54:13 +0900

    [Bronze IV] Title: R2, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();   //평균
+
+        int R2 = S * 2 - R1;
+
+        System.out.println(R2);
+    }
+}
