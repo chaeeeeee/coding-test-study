6e0c82b3e1324836f7bf2c6cb4b3224e53721bdd
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-02-17 22:54:32 +0900

    [Bronze II] Title: 분산처리, Time: 1228 ms, Memory: 21436 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int a, b, r;
+
+        for (int i = 0; i < T; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            r = 1;
+
+            for (int j = 0; j < b; j++) {
+                r = (r * a) % 10;
+            }
+            if (r == 0) {
+                r = 10;
+            }
+            System.out.println(r);
+        }
+    }
+}
