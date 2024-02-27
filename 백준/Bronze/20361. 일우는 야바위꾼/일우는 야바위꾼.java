a7d3f9d7bedca1f7e1377b6106c9bb8f38b10363
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-02-27 20:31:26 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 1132 ms, Memory: 184896 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int K = sc.nextInt();
+        
+        for (int i = 0; i < K; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            if (A == X) {
+                X = B;
+            } else if (B == X) {
+                X = A;
+            }
+        }
+
+        System.out.println(X);
+    }
+}
