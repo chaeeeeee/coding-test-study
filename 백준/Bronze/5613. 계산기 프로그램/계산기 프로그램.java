450e0efc38e9902beeefe722280ab2d3ee9afc16
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-03-21 22:06:31 +0900

    [Bronze III] Title: 계산기 프로그램, Time: 224 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/5613. 계산기 프로그램/계산기 프로그램.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int sum = sc.nextInt();
+        
+        while (true) {
+            String str = sc.nextLine();
+            if (str.equals("=")) {
+                break;
+            } 
+            
+            switch (str) {
+                case "+":
+                    sum += sc.nextInt();
+                    break;
+                case "-":
+                    sum -= sc.nextInt();
+                    break;
+                case "*":
+                    sum *= sc.nextInt();
+                    break;
+                case "/":
+                    sum /= sc.nextInt();
+                    break;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
