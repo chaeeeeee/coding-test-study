0e7c1fde4d1861c3146b552335caea4b6e5f1275
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-06-28 22:44:43 +0900

    [Bronze V] Title: 카드 게임, Time: 124 ms, Memory: 14108 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int total = 0;
+        
+        for (int i = 1; i <= 5; i++) {
+            total += Integer.parseInt(br.readLine());
+        }
+
+        System.out.print(total);
+    }
+}
