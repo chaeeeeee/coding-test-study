ee6da38531904c7336f9b1e255e5e0b307cf9a0c
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-08-03 22:23:50 +0900

    [Silver II] Title: 이항 계수 2, Time: 136 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ", false);
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N + 1][N + 1];
+        
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == j || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+                }
+            }
+        }
+        
+        bw.write(dp[N][K] + "");
+        bw.close();
+    }
+}
