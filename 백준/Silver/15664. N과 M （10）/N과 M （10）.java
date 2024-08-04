ba69cfe60da6585bc1e3995fec8b4fb17ea875e0
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-08-04 11:08:00 +0900

    [Silver II] Title: N과 M (10), Time: 188 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr, printArr;
+    static boolean[] isVisited;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[N];
+        printArr = new int[M];
+        isVisited = new boolean[N];
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+        
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+    
+    static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(printArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        int before = -1;
+        for (int i = start; i < N; i++) {
+            int now = arr[i];
+            if (before == now || isVisited[i]) {
+                continue;
+            } else {
+                before = now;
+                isVisited[i] = true;
+                printArr[depth] = arr[i];
+                dfs(i + 1, depth + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+}
