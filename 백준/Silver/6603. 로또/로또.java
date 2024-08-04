54144b9873e05e6e80b0afc9e1d9062997f4b375
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2024-08-04 11:03:06 +0900

    [Silver II] Title: 로또, Time: 308 ms, Memory: 19428 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+
+    static int K, S, cnt = 0;
+    static int[] num;
+    static boolean[] check;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            K = sc.nextInt();
+            
+            if (K == 0) {
+                break;
+            }
+            num = new int[K];
+            check = new boolean[K];
+            
+            for (int i = 0; i < K; i++) {
+                num[i] = sc.nextInt();
+            }
+            f(0, 0);
+            System.out.println();
+        }
+    }
+    
+    static void f(int dp, int start) {
+        if (dp == 6) {
+            for (int i = 0; i < K; i++) {
+                if (check[i]) {
+                    System.out.print(num[i] + " ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = start; i < K; i++) {
+            check[i] = true;
+            f(dp + 1, i + 1);
+            check[i] = false;
+        }
+    }
+}
